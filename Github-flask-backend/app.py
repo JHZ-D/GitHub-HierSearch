b25a0bcf32892b439e87b048c74c58425619f1e9
JHZ-D <1336024978@qq.com>
2023-05-31 15:18:22 +0800

    clear

--- Github-flask-backend/app.py
+++ Github-flask-backend/app.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from scripts import tabledef
-# from scripts import forms
-# from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, request
 from flask_cors import CORS
 import json
-import sys
 import os
 import re
-import copy
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -39,9 +34,6 @@ with open('common/ReposforSearch.json', 'r') as f:
 with open('common/topicrepo.json', 'r') as f:
     topicrepo = json.load(f)
 
-# Heroku
-#from flask_heroku import Heroku
-#heroku = Heroku(app)
 
 @app.after_request
 def af_request(resp):     
@@ -55,152 +47,6 @@ def af_request(resp):
     resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
     resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
     return resp
-
-
-# ======== Routing =========================================================== #
-# @app.route('/', methods=['GET', 'POST'])
-
-
-# -------- Login ------------------------------------------------------------- #
-# def login():
-#     if not session.get('logged_in'):
-#         form = forms.LoginForm(request.form)
-#         if request.method == 'POST':
-#             username = request.form['username'].lower()
-#             password = request.form['password']
-#             if form.validate():
-#                 if helpers.credentials_valid(username, password):
-#                     session['logged_in'] = True
-#                     session['username'] = username
-#                     return json.dumps({'status': 'Login successful'})
-#                 return json.dumps({'status': 'Invalid user/pass'})
-#             return json.dumps({'status': 'Both fields required'})
-#         return render_template('login.html', form=form)
-#     user = helpers.get_user()
-#     return render_template('home.html', user=user)
-
-
-# @app.route("/logout")
-# def logout():
-#     session['logged_in'] = False
-#     return redirect(url_for('login'))
-
-
-# -------- Signup ---------------------------------------------------------- #
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     if not session.get('logged_in'):
-#         form = forms.LoginForm(request.form)
-#         if request.method == 'POST':
-#             username = request.form['username'].lower()
-#             password = helpers.hash_password(request.form['password'])
-#             email = request.form['email']
-#             if form.validate():
-#                 if not helpers.username_taken(username):
-#                     helpers.add_user(username, password, email)
-#                     session['logged_in'] = True
-#                     session['username'] = username
-#                     return json.dumps({'status': 'Signup successful'})
-#                 return json.dumps({'status': 'Username taken'})
-#             return json.dumps({'status': 'User/Pass required'})
-#         return render_template('login.html', form=form)
-#     return redirect(url_for('login'))
-
-
-# -------- Settings ---------------------------------------------------------- #
-# @app.route('/settings', methods=['GET', 'POST'])
-# def settings():
-#     if session.get('logged_in'):
-#         if request.method == 'POST':
-#             password = request.form['password']
-#             if password != "":
-#                 password = helpers.hash_password(password)
-#             email = request.form['email']
-#             helpers.change_user(password=password, email=email)
-#             return json.dumps({'status': 'Saved'})
-#         user = helpers.get_user()
-#         return render_template('settings.html', user=user)
-#     return redirect(url_for('login'))
-
-cut = set()
-def dfsknowgraph(tree, id, repos, depth = -1):
-    newtree = copy.deepcopy(tree)
-    cut.add(tree['id'])
-    if depth == 0:
-        for child in tree['children']:
-            if child['id'].startswith('rp'):
-                if child['id'] in repos:
-                    newtree['children'].remove(child)
-            else:
-                newtree['children'].remove(child)
-        newtree['children'] = newtree['children'][:5]
-        return newtree
-    
-    if depth == 1:
-        cnt = 0
-        newtree['children'] = []
-        newrepos = copy.deepcopy(repos)
-        for child in tree['children']:
-            if child['id'].startswith('rp') and not child['id'] in repos:
-                newtree['children'].append(child)
-                cnt += 1
-                newrepos.append(child['id'])
-                if cnt == 5:
-                    break
-        for child in tree['children']:
-            if not child['id'].startswith('rp'):
-                newtree['children'].append(dfsknowgraph(child, child['id'], newrepos, 0))
-        return newtree
-    
-    if depth == 2:
-        cnt = 0
-        newtree['children'] = []
-        newrepos = []
-        for child in tree['children']:
-            if child['id'].startswith('rp'):
-                newtree['children'].append(child)
-                cnt += 1
-                newrepos.append(child['id'])
-                if cnt == 5:
-                    break
-        for child in tree['children']:
-            if not child['id'].startswith('rp'):
-                newtree['children'].append(dfsknowgraph(child, child['id'], newrepos, 0))
-        return newtree
-
-    if tree['id'] == id:
-        cnt = 0
-        newtree['children'] = []
-        newrepos = []
-        for child in tree['children']:
-            if child['id'].startswith('rp'):
-                newtree['children'].append(child)
-                cnt += 1
-                newrepos.append(child['id'])
-                if cnt == 5:
-                    break
-        for child in tree['children']:
-            if not child['id'].startswith('rp'):
-                newtree['children'].append(dfsknowgraph(child, child['id'], newrepos, 1))
-        return newtree
-    if not 'children' in tree:
-        return None
-    for child in tree['children']:
-        if child['id'] in cut:
-            continue
-        if (child['id'].startswith('tp') or child['id'].startswith('ts')) and (id.startswith('pg') or id[0].isdigit()):
-            continue
-        if child['id'].startswith('pg') and id[0].isdigit():
-            continue
-        if child['id'] == id:
-            newtree['children'] = [dfsknowgraph(child, child['id'], [], 2)]
-            return newtree
-        else:
-            dfsre = dfsknowgraph(child, id, [], -1)
-            if dfsre != None:
-                print(tree['id'])
-                return dfsre
-    return None
 
 def getGraph(id, parentid):
     curitem = wholedict[id]
@@ -271,15 +117,12 @@ def getKnowp():
     wikibaseurl = 'https://en.wikipedia.org/wiki/'
     topicbaseurl = 'https://github.com/topics/'
     repobaseurl = 'https://github.com/'
-    print('base!')
     graph = {}
-    cut.clear()
     if nodeid.startswith('rp'):
         desc = repos_info[nodeid[2:]]['description']
         url = repobaseurl + repos_info[nodeid[2:]]['full_name']
         graph = getRepoGraph(nodeid)
     else:
-        # graph = dfsknowgraph(wholegraphtree, nodeid, [])
         graph = getGraph(nodeid, parentid)
         if nodeid.startswith('pg'):
             desc = pageinfo[nodeid[2:]]['desc']
@@ -330,19 +173,12 @@ def MatchingRepos(keywords, repos):  # 输入为列表
     for info in repos:
         if keywords.issubset(set(info['content'])):
             matchingRepos.append(info)
-        # for keyword in keywords:
-        #     if not (keyword in reponame or keyword in description):
-        #         m = 1
-        #         break
-        # if m == 0:
-        #     matchingRepos.append(info)
     # sort matchingRepos by number of stars
     matchingRepos.sort(key=lambda x: x['stargazers_count'], reverse=True)
     return matchingRepos
 
 @app.route('/repo', methods=['GET'])
 def getRepo():
-    # print(request.args)
     searchText = request.args.get('text')
     cateid = request.args.get('cate')
     if cateid != '0':
